e300901ff5e33c4137bbabbdedfa5786297d6832
shiryu-nakano <shiryu.nakano03@gmail.com>
2025-06-08 14:38:04 +0900

    commit

--- include/my_lifecycle_tutorial/supervisor_component.hpp
+++ include/my_lifecycle_tutorial/supervisor_component.hpp
@@ -16,10 +16,10 @@ class SupervisorComponent
 public:
     // 2点間距離計算
     static double CalcDistance(const Pose2D& a, const Pose2D& b);
-
     // 閾値判定：十分近ければtrue
     static bool IsWithinThreshold(const Pose2D& a, const Pose2D& b, double threshold);
     
+
     // 追加でスカラー値比較や他の演算が必要なら随時追加
 };
 
